a3d0124ea5d10b8dee15d631110d1389a4e426b8
Elisa <elisaashryver@hotmail.com>
2023-01-02 21:53:15 +0100

    add config properties

--- src/MyAnimeWatchListTool/Configuration/Config.cs
+++ src/MyAnimeWatchListTool/Configuration/Config.cs
@@ -2,5 +2,7 @@
 
 public class Config
 {
-    
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+    public string MongoConnection { get; set; } = string.Empty;
 }
