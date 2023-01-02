f781d3a938e51a00bf1d4e7e042c2baf80247ca4
Elisa <elisaashryver@hotmail.com>
2023-01-02 22:04:12 +0100

    add SnakeCaseNamingPolicy

---
+++ src/MyAnimeWatchListTool/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace MyAnimeWatchListTool.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSnakeCase(this string str)
+    {
+        return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
+    }
+}
